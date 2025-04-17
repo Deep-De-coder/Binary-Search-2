b71fb77664ac81fa3c4585b3160ffed8328b92b5
Deep Shahane <61946392+Deep-De-coder@users.noreply.github.com>
2025-04-16 20:56:45 -0400

    Code Updated

---
+++ P2.py
@@ -0,0 +1,13 @@
+#TIme Complexityy :O(logn)
+# Space COmplexity : O(1)
+# Able to run on leetcode :Yes
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l,r = 0,len(nums)-1
+        while l<r:
+            mid=(l+r)//2
+            if nums[mid]>nums[r]:
+                l=mid+1
+            else:
+                r=mid
+        return nums[l]
